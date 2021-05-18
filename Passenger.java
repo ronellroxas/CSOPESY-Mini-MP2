2cd4c3eacf68b18d0f2c3097c5ac77f483c0bf84
howardg123 <60571748+howardg123@users.noreply.github.com>
2021-05-18 15:50:41 +0800

    added semaphore

--- Passenger.java
+++ Passenger.java
@@ -1,26 +1,28 @@
 import java.util.*;
+import java.util.concurrent.Semaphore;
 
 class Passenger extends Thread {
 
     protected int id;
-    protected Queue<Car> cars; //kachow
-    protected Queue<Car> doneCars; //kachow
+    protected Queue<Car> cars; // kachow
+    protected Queue<Car> doneCars; // kachow
     private int riding;
     private Random rand = new Random();
+    private Semaphore mutex = new Semaphore(1);
 
     @Override
     public void run() {
-        while(true) {
-            if(riding == -1) {
+        while (true) {
+            if (riding == -1) {
                 wander();
                 board();
                 unboard();
 
-                if(cars.peek() == null && doneCars.peek() == null) {
+                if (cars.peek() == null && doneCars.peek() == null) {
                     break;
                 }
             }
-            
+
         }
     }
 
@@ -34,9 +36,8 @@ class Passenger extends Thread {
     private void wander() {
         try {
             System.out.println("Passenger " + id + " is wandering.");
-            Thread.sleep((rand.nextInt(10) + 2)* 1000);
-        }
-        catch(Exception e) {
+            Thread.sleep((rand.nextInt(10) + 2) * 1000);
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
@@ -47,51 +48,50 @@ class Passenger extends Thread {
     private void board() {
         try {
             Car car = cars.peek();
-            if(car.getStatus().equalsIgnoreCase("load") && car.notFull()) {
+            if (car.getStatus().equalsIgnoreCase("load") && car.notFull()) {
                 riding = car.carId();
                 car.loadPassenger();
+                mutex.acquire();
                 System.out.println("Passenger " + id + " boarded Car " + riding + ".");
 
-                if(!car.notFull()) {
-                    Thread.sleep(1*1000);   //sync to Car run
+                if (!car.notFull()) {
+                    Thread.sleep(1 * 1000); // sync to Car run
                     doneCars.add(cars.remove());
                     cars.peek().setStatus("load");
                 }
             }
-            Thread.sleep(10*1000);
-        }
-        catch(Exception e) {
+            Thread.sleep(10 * 1000);
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
     private void unboard() {
         try {
-            while(true) {
+            while (true) {
                 Car car = doneCars.peek();
-                if(car != null && riding != -1) {
-                    
-                    while(!car.getStatus().equalsIgnoreCase("unload")) {
+                if (car != null && riding != -1) {
+
+                    while (!car.getStatus().equalsIgnoreCase("unload")) {
                         //
                     }
 
-                    if(car.carId() == riding) {
+                    if (car.carId() == riding) {
                         car.unloadPassenger();
+                        mutex.release();
                         System.out.println("Passenger " + id + " unboarded Car " + car.carId() + ".");
                         riding = -1;
-                        
 
-                        if(car.getPassenger() == 0)
+                        if (car.getPassenger() == 0)
                             doneCars.remove();
-                        
+
                     }
-                    if(riding == -1) {
+                    if (riding == -1) {
                         break;
                     }
                 }
             }
-        }
-        catch(Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
